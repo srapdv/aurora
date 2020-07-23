47f09d793b1ce44614b223c602bef5fd449b60cc
jego.ramos <jego.ramos@blackpearl.technology>
2020-07-23 13:38:51 +0800

    before device_manager

--- aurora/aurora.py
+++ aurora/aurora.py
@@ -3,6 +3,7 @@ __author__ = "Jego Carlo Ramos, Simoun De Vera"
 __copyright__ = "Copyright (C) 2020, Blackpearl Technology"
 __maintainer__ = "Simoun De Vera"
 
+
 from customization import CustomizationRunner
 
 print("Hello")
